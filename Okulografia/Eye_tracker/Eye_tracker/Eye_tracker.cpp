6cef7eef523b86db6b3130e060ebcd651d23ccee
patrycja_\patrycja <patrycja.sliwinska@student.put.poznan.pl>
2017-12-16 23:54:35 +0100

    Dodanie odbicia lustrzanego

--- Okulografia/Eye_tracker/Eye_tracker/Eye_tracker.cpp
+++ Okulografia/Eye_tracker/Eye_tracker/Eye_tracker.cpp
@@ -221,18 +221,141 @@ int main() {
 
 */
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2\videoio.hpp>
 #include <opencv2\highgui.hpp>
 #include "opencv2/imgproc/imgproc.hpp" 
 #include <string> 
 #include <iostream>
 #include <fstream>
+#include "popt_pp1.h"
+#include "stdafx.h"
+//#include <Windows.h>
 using namespace cv;
 using namespace std;
 
-void main()
+//kali
+
+vector< vector< Point3f > > object_points;
+vector< vector< Point2f > > image_points;
+vector< Point2f > corners;
+vector< vector< Point2f > > left_img_points;
+
+Mat img, gray;
+Size im_size;
+
+void setup_calibration(int board_width, int board_height, int num_imgs,
+	float square_size, char* imgs_directory, char* imgs_filename,
+	char* extension) {
+	Size board_size = Size(board_width, board_height);
+	int board_n = board_width * board_height;
+
+	for (int k = 1; k <= num_imgs; k++) {
+		char img_file[100];
+		sprintf(img_file, "%s%s%d.%s", imgs_directory, imgs_filename, k, extension);
+		img = imread(img_file, CV_LOAD_IMAGE_COLOR);
+		cv::cvtColor(img, gray, CV_BGR2GRAY);
+
+		bool found = false;
+		found = cv::findChessboardCorners(img, board_size, corners,
+			CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_FILTER_QUADS);
+		if (found)
+		{
+			cornerSubPix(gray, corners, cv::Size(5, 5), cv::Size(-1, -1),
+				TermCriteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 30, 0.1));
+			drawChessboardCorners(gray, board_size, corners, found);
+		}
+
+		vector< Point3f > obj;
+		for (int i = 0; i < board_height; i++)
+			for (int j = 0; j < board_width; j++)
+				obj.push_back(Point3f((float)j * square_size, (float)i * square_size, 0));
+
+		if (found) {
+			cout << k << ". Found corners!" << endl;
+			image_points.push_back(corners);
+			object_points.push_back(obj);
+		}
+	}
+}
+
+double computeReprojectionErrors(const vector< vector< Point3f > >& objectPoints,
+	const vector< vector< Point2f > >& imagePoints,
+	const vector< Mat >& rvecs, const vector< Mat >& tvecs,
+	const Mat& cameraMatrix, const Mat& distCoeffs) {
+	vector< Point2f > imagePoints2;
+	int i, totalPoints = 0;
+	double totalErr = 0, err;
+	vector< float > perViewErrors;
+	perViewErrors.resize(objectPoints.size());
+
+	for (i = 0; i < (int)objectPoints.size(); ++i) {
+		projectPoints(Mat(objectPoints[i]), rvecs[i], tvecs[i], cameraMatrix,
+			distCoeffs, imagePoints2);
+		err = norm(Mat(imagePoints[i]), Mat(imagePoints2), CV_L2);
+		int n = (int)objectPoints[i].size();
+		perViewErrors[i] = (float)std::sqrt(err*err / n);
+		totalErr += err*err;
+		totalPoints += n;
+	}
+	return std::sqrt(totalErr / totalPoints);
+}
+//kali
+
+//void main(
+	int main(int argc, char* argv[])
 {
-	
+		/*
+		//kali
+		int board_width, board_height, num_imgs;
+		float square_size;
+		char* imgs_directory;
+		char* imgs_filename;
+		char* out_file;
+		char* extension;
+
+		static struct poptOption options[] = {
+			{ "board_width",'w',POPT_ARG_INT,&board_width,0,"Checkerboard width","NUM" },
+			{ "board_height",'h',POPT_ARG_INT,&board_height,0,"Checkerboard height","NUM" },
+			{ "num_imgs",'n',POPT_ARG_INT,&num_imgs,0,"Number of checkerboard images","NUM" },
+			{ "square_size",'s',POPT_ARG_FLOAT,&square_size,0,"Size of checkerboard square","NUM" },
+			{ "imgs_directory",'d',POPT_ARG_STRING,&imgs_directory,0,"Directory containing images","STR" },
+			{ "imgs_filename",'i',POPT_ARG_STRING,&imgs_filename,0,"Image filename","STR" },
+			{ "extension",'e',POPT_ARG_STRING,&extension,0,"Image extension","STR" },
+			{ "out_file",'o',POPT_ARG_STRING,&out_file,0,"Output calibration filename (YML)","STR" },
+			POPT_AUTOHELP
+		{ NULL, 0, 0, NULL, 0, NULL, NULL }
+		};
+
+		POpt popt(NULL, argc, argv, options, 0);
+		int c;
+		while ((c = popt.getNextOpt()) >= 0) {}
+
+		setup_calibration(board_width, board_height, num_imgs, square_size,imgs_directory, imgs_filename, extension);
+
+		printf("Starting Calibration\n");
+		Mat K;
+		Mat D;
+		vector< Mat > rvecs, tvecs;
+		int flag = 0;
+		flag |= CV_CALIB_FIX_K4;
+		flag |= CV_CALIB_FIX_K5;
+		calibrateCamera(object_points, image_points, img.size(), K, D, rvecs, tvecs, flag);
+
+		cout << "Calibration error: " << computeReprojectionErrors(object_points, image_points, rvecs, tvecs, K, D) << endl;
+
+		FileStorage fs(out_file, FileStorage::WRITE);
+		fs << "K" << K;
+		fs << "D" << D;
+		fs << "board_width" << board_width;
+		fs << "board_height" << board_height;
+		fs << "square_size" << square_size;
+		printf("Done Calibration\n");
+
+		//kali
+		*/
+
 	fstream wyniki,w;	// Tworzenie kana³ów dla plików
 	const uint CAM_NUM = 2;		//Liczba kamer
 	
@@ -249,11 +372,13 @@ void main()
 		int frame_height0 = kanalczolo.get(CV_CAP_PROP_FRAME_HEIGHT);
 		int frame_width1 = kanaloko.get(CV_CAP_PROP_FRAME_WIDTH); //do zapisu wymiary
 		int frame_height1 = kanaloko.get(CV_CAP_PROP_FRAME_HEIGHT);
+		cout << "Czolo szer. " << frame_width0 << ", wys. " << frame_height0 << endl;
+		cout << "Oko szer. " << frame_width1 << ", wys. " << frame_height1 << endl;
 		// Define the codec and create VideoWriter object.The output is stored in 'outcpp.avi' file. 
-		VideoWriter video0("Czolo.avi", CV_FOURCC('M', 'J', 'P', 'G'), 10, Size(frame_width0, frame_height0));
-		VideoWriter video1("Oko.avi", CV_FOURCC('M', 'J', 'P', 'G'), 10, Size(frame_width1, frame_height1));
-		VideoWriter video2("Czolo_z_okregami.avi", CV_FOURCC('M', 'J', 'P', 'G'), 10, Size(frame_width0, frame_height0));
-		VideoWriter video3("Analiza.avi", CV_FOURCC('M', 'J', 'P', 'G'), 10, Size(frame_width0, frame_height0));
+		VideoWriter video0("Czolo.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(frame_width0, frame_height0));
+		VideoWriter video1("Oko.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(frame_width1, frame_height1));
+		VideoWriter video2("Czolo_z_okregami.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(frame_width0, frame_height0));
+		VideoWriter video3("Analiza.avi", CV_FOURCC('M', 'J', 'P', 'G'), 5, Size(frame_width0, frame_height0));
 		/*
 		//znalezienie parametrów filtra
 		namedWindow("Control", CV_WINDOW_AUTOSIZE);
@@ -287,10 +412,11 @@ void main()
 			video0.write(RamkaCzolo);
 			imshow("Obraz z czola", RamkaCzolo);
 
-			kanaloko >> RamkaOko;  // przechywtywanie klatka po klatce z ka¿dego ujêcia
+			kanaloko >> RamkaOko;  // przechwtywanie klatka po klatce z ka¿dego ujêcia
 
 			//Obrót obrazu
-			Mat MacierzRotacji = getRotationMatrix2D(Point(RamkaOko.cols / 2, RamkaOko.rows / 2), 270, 1);
+			Mat MacierzRotacji = getRotationMatrix2D(Point(RamkaOko.cols / 2, RamkaOko.rows / 2), 260, 1);
+			//Mat MacierzRotacji = getRotationMatrix2D(Point(RamkaOko.cols, RamkaOko.rows), 270, 1);
 			Mat RamkaRotacji;
 			warpAffine(RamkaOko, RamkaRotacji, MacierzRotacji, RamkaOko.size());
 			RamkaOko = RamkaRotacji;
@@ -332,20 +458,16 @@ void main()
 				int radius = cvRound(circles[i][2]);
 				
 				wyniki << nr << "; " << cvRound(circles[i][0]) << "; " << cvRound(circles[i][1]) << "; " << cvRound(circles[i][2]) << endl;
-
-				circle(img0, center, 3, Scalar(0, 255, 0), -1, 8, 0); //œreodek okrêgu
+				img = img0;
+				flip(img, img0, 1);//odwracanie obrazu wzglêdem y
+				
+				circle(img0, center, 3, Scalar(0, 255, 0), -1, 8, 0); //œrodek okrêgu
 				circle(img0, center, radius, Scalar(0, 0, 255), 3, 8, 0); // kontur okrêgu
 				//czolo
 				circle(czolo, center, 30, Scalar(0, 255, 0), -1, 8, 0);
 				circle(czolo, center, radius, Scalar(0, 0, 255), 3, 8, 0);
 				//ellipse(img0, center, s, double angle, double startAngle, double endAngle, const Scalar& color, int thickness = 1, int lineType = 8, int shift = 0)¶
 			}
-
-
-
-
-
-
 
 /*
 			vector<vector<Point> > contours;
@@ -358,7 +480,6 @@ void main()
 				rotRecs[i] = fitEllipse(contours[i]);
 			}
 */
-
 
 			// Rysowanie okrêgów na kamerze
 			namedWindow("Okregi", CV_WINDOW_AUTOSIZE);
@@ -482,7 +603,7 @@ void main()
 			{
 				if (nr == tabwy[i][0])
 				{
-					srx = tabwy[i][1];
+					srx = 640 - tabwy[i][1];
 					sry = tabwy[i][2];
 				}
 			}
@@ -506,12 +627,12 @@ void main()
 			Point srodek4(srx4, sry4);
 			Point srodek5(srx5, sry5);
 
-			circle(analiza, srodek5, 10, Scalar(0, 100, 0), -1, 8, 0);
-			circle(analiza, srodek4, 14, Scalar(0, 125, 0), -1, 8, 0);
-			circle(analiza, srodek3, 18, Scalar(0, 150, 0), -1, 8, 0);
-			circle(analiza, srodek2, 22, Scalar(0, 175, 0), -1, 8, 0);
-			circle(analiza, srodek1, 25, Scalar(0, 215, 0), -1, 8, 0);
-			circle(analiza, srodek, 30, Scalar(0, 255, 0), -1, 8, 0);
+			circle(analiza, srodek5, 10, Scalar(0, 100, 0), 4, 0, 0);
+			circle(analiza, srodek4, 14, Scalar(0, 125, 0), 4, 0, 0);
+			circle(analiza, srodek3, 18, Scalar(0, 150, 0), 4, 0, 0);
+			circle(analiza, srodek2, 22, Scalar(0, 175, 0), 4, 0, 0);
+			circle(analiza, srodek1, 25, Scalar(0, 215, 0), 4, 0, 0);
+			circle(analiza, srodek, 30, Scalar(0, 255, 0), 4, 0, 0);
 
 
 			if (ramka != 0) // Je¿eli nie jest pusta to wyœwietlamy
